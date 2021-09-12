48499eb82f753c189d16bd20652fc2306585d18b
JanChelmecki <janch2004@gmail.com>
2021-09-12 20:38:25 +0100

    Done

---
+++ StartUpPython/2.Password.py
@@ -0,0 +1,5 @@
+password = input("Choose a password: ")
+if len(password)>6:
+    print("Password is valid.")
+else:
+    print("Password is invalid.")
---
+++ StartUpPython/4.Division.py
@@ -0,0 +1,3 @@
+n = int(input("Integer: "))
+k = int(input("Divisor: "))
+print(n//k, "remainder", n%k)
